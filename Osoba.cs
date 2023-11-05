5fdc354d600fcbce5c9c4f6d811a57b4265c30d4
MarketaHorakova <99902734+MarketaHorakova@users.noreply.github.com>
2023-11-05 17:01:47 +0100

    Add project files.

---
+++ Osoba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KartotekaOsob
+
+{
+    public class Person
+    {
+        public string Name;
+        public string Surname;
+        public string BirthDate;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using KartotekaOsob;
+
+List<Person> persons = new List<Person>();
+
+bool isOver = false;
+
+while (!isOver)
+{
+    Console.WriteLine("1 - Add person");
+    Console.WriteLine("2 - Delete person");
+    Console.WriteLine("3 - Show persons");
+    Console.WriteLine("0 - Konec");
+
+    int choosenNumber = Convert.ToInt32(Console.ReadLine());
+
+    switch (choosenNumber)
+    {
+        case 0:
+            isOver =true; 
+            break;
+        case 1:
+            Person johnDoe = new Person();
+            Console.Write("Input the name: ");
+            johnDoe.Name = Console.ReadLine();
+            Console.Write("Input the surname: ");
+            johnDoe.Surname = Console.ReadLine();
+            Console.Write("Input the bitrh date: ");
+            johnDoe.BirthDate = Console.ReadLine();
+            persons.Add(johnDoe);
+            break; 
+        case 2:
+            Console.WriteLine("Zadej index mazane polozky: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            persons.RemoveAt(index);
+            break;
+        case 3:
+            int i = 0;
+            foreach (Person onePerson in persons)
+            {
+                Console.WriteLine($"{i}\t{onePerson.Name}\t\t{onePerson.Surname}\t\t{onePerson.BirthDate}");
+                i++;
+            }
+            break;
+    }
+
+
+
+}
+
+
+
+Console.ReadLine();
+
